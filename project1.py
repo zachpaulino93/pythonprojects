f9387cc21b828cbc236d4db7ee6c9d04804baea4
ZacharyP <zachpaulino93@gmail.com>
2020-11-13 22:10:23 -0500

    fixed

--- project1.py
+++ project1.py
@@ -22,28 +22,6 @@ rotated_image.save("file_rotated.png") # Save the rotated image.
 
 #os directory because folder is a directory not a file!
 
-from PIL import Image
-import os
-
-def main():
-    path = '/home/student-00-79bb12745b0f/images'
-    for image_path in os.listdir(path):
-        output_path = os.path(future path here)
-        input_path = os.path.join(path, image_path)
-        img = Image.open(input_path)
-        print("Rotating image")
-        rotated = img.rotate(-90) # .save(input_path + "_new.png")
-        print("rotating complete")
-        print()
-        print("Scailing")
-        scailing = rotated.resize((128,128))
-        print("scailing completed")
-        new_image = scailing.save(output_path + ".png")
-        print("\n saving file")
-        new_image.close
-if __name__ == '__main__':
-    main()
-
 #!/usr/bin/env python3
 from PIL import Image
 import os
