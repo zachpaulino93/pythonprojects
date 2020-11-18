865be948f0c8d0b850c585c56555ed533fab1290
ZacharyP <zachpaulino93@gmail.com>
2020-11-17 19:36:10 -0500

    modifed project

--- project3.py
+++ project3.py
@@ -27,7 +27,7 @@ def load_smtp(sender_email, message):
     print("email sent")
     mail_server.quit()
 
-def create_email(sender_email, recipient):
+def create_email(sender_email, recipient, attachment):
     sender = sender_email
     recipient = recipient
     # setting to and from portion
@@ -40,24 +40,30 @@ def create_email(sender_email, recipient):
     out! """
     # setting variable and putting body into a function
     message.set_content(body)
+    message.add_attachment(attachment)
     return message
-"""
-def get_file()
-    attachment_path = /"whatever_path.png"
-    attachment_filename = os.path.basename(attachment_path)
-    mime_type, _ = mimetypes.guess_type(attachment_path)
-    mime_type, mime_subtype = mime_subtype.split('/', 1)
-    with open(attachment_path, 'rb') as ap:
-        maintype=mime_type,
-        subtype=mime_subtype,
-        filename=os.path.basename(attachment_path)
 
-   return ap
-"""
+def get_attachy()
+    # full path here
+    attachment_path = "/tmp/example.png"
+    # basename path just returns the end why is this even here?
+    attachment_filename = os.path.basename(attachment_path)
+    # getting mimetype for adding later
+    mime_type _= mimetypes.guess(attachment_path)
+    sub_type = subtype = mime_type.split('/', 1)
+    # opening image here with full path
+    with open(attachment_path 'rb') as ap
+        message.add_attachment(ap.read(),
+            maintype=mime_type,
+            subtype=mime_subtype,
+            filename=os.path.basename(attachment_path))
+    return attachment
+
+
 
 if __name__ == "__main__":
     sender_email, recipient = setup_users()
-    email = create_email(sender_email, recipient)
+    email = create_email(sender_email, recipient, attachment)
     print("gathering email data to complie")
     print("Starting program, will initiate load_smtp")
     smtp_server = load_smtp(sender_email, email)
